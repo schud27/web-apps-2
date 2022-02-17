848e7d1c7f83cf996d81cb288f33433ff96358bf
schud27 <dan.schulte@kellogg.northwestern.edu>
2022-02-17 01:17:25 +0000

    added form for name intake

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -2,6 +2,7 @@ class DiceController < ApplicationController
     def index
         @die1 = rand(1..6)
         @die2 = rand(1..6)
-        total = @die1 + @die2
+        @total = @die1 + @die2
+        @name = params["name"]
     end
 end
